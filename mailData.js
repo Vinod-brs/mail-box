ce80b838e005f550ab315554a84870abe629d549
Vinod-brs <kingvinodkumart@gmail.com>
2024-12-24 15:58:55 +0530

    first commit

---
+++ mailData.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Nyros Technologies',
+    from: 'admin@nyros.com',
+    subject: 'React JS Tutorial',
+    content: '<p>Hi there,</p><p>We’re excited to share that we will be participating in Collision 2025 in Toronto!</p><p>Collision brings together people and companies, redefining the global tech industry. Politico describes it as the ‘Olympics of Tech.’</p><p>Here’s what we have planned:</p><p>Stop by our booth #E222 (Hall B) to speak with our team, discuss your tech stack, and learn how you could receive up to $100K USD worth of Cloud credits on us (T&C’s apply).</p><p>Listen to our VP of Product Marketing, Raman Sharma, on ‘Building Great Products: Guided by Customer Feedback’ during the Masterclass on January 27th at 11:00 AM.</p><p>Our team will also be hosting office hours each day! Consider booking a 1:1 meeting with our team ahead of time to talk about how DigitalOcean can help your business succeed.</p><p>Not attending Collision 2025? Then sign up to join us at the DigitalOcean x CTO.ai Happy Hour on Jaunary 20th.</p><p>Register now</p><p>So mark your calendars. We look forward to seeing you there!</p><p>Swimmingly,</p><p>The DigitalOcean Team</p>',
+    date: 'Today',
+    isRead: false,
+    isArchived: false
+  },
+  {
+    id: 2,
+    name: 'Jeff Calendly',
+    from: 'jeff@calendly.com',
+    content: '<p>Hi Manohar,</p><p>This is Jeff from Calendly with not only the written word, but also an invitation to a live event full of tips on scheduling as a team!</p><p><h2>Tip of the Day… Live on January 1!</h2></p><p>We often share tips here for scheduling better as a team:</p><ul><li>Collective scheduling for bringing a coworker like a sales engineer or recruiter to a meeting (Professional plan and above)</li><li>Round Robin for sharing meeting opportunities with coworkers — like a pool of salespeople or customer-success pros (Teams plan and above)</li></ul><p>Thanks for reading,</p><p>Calendly Headson</p><p>Head of Product Marketing @ Calendly</p>',
+    subject: 'NodeJS Tutorial',
+    date: 'Yesterday',
+    isRead: true,
+    isArchived: false
+  },
+  {
+    id: 3,
+    name: 'Manohar Nyros',
+    from: 'manohar.nyros@gmail.com',
+    content: '<p>Hi there,</p><p>We’re excited to share that we will be participating in Collision 2023 in Toronto!</p><p>Collision brings together people and companies, redefining the global tech industry. Politico describes it as the ‘Olympics of Tech.’</p><p>Here’s what we have planned:</p><p>Stop by our booth #E222 (Hall B) to speak with our team, discuss your tech stack, and learn how you could receive up to $100K USD worth of Cloud credits on us (T&C’s apply).</p><p>Listen to our VP of Product Marketing, Raman Sharma, on ‘Building Great Products: Guided by Customer Feedback’ during the Masterclass on June 27th at 11:00 AM.</p><p>Our team will also be hosting office hours each day! Consider booking a 1:1 meeting with our team ahead of time to talk about how DigitalOcean can help your business succeed.</p><p>Not attending Collision 2023? Then sign up to join us at the DigitalOcean x CTO.ai Happy Hour on June 28th.</p><p>Register now</p><p>So mark your calendars. We look forward to seeing you there!</p><p>Swimmingly,</p><p>The DigitalOcean Team</p>',
+    subject: 'Js Tutorial',
+    date: 'Dec 20',
+    isRead: true,
+    isArchived: false
+  },
+  {
+    id: 4,
+    name: 'Vinod',
+    from: 'Vinod@nyros.com',
+    content: '<p>Hi there,</p><p>This is Vinod from Kakinada with not only the written word, but also an invitation to a live event full of tips on scheduling as a team!</p><p><h2>Tip of the Day… Live on Dec 22!</h2></p><p>We often share tips here for scheduling better as a team:</p><ul><li>Collective scheduling for bringing a coworker like a sales engineer or recruiter to a meeting (Professional plan and above)</li><li>Round Robin for sharing meeting opportunities with coworkers — like a pool of salespeople or customer-success pros (Teams plan and above)</li></ul><p>Thanks for reading,</p><p>Jeff Hardison</p><p>Head of Product Marketing @ Calendly</p>',
+    subject: 'Full stack',
+    date: 'Dec 11',
+    isRead: false,
+    isArchived: false
+  },
+  {
+    id: 5,
+    name: 'Vinod Kumar V',
+    from: 'vinodkumar@nyros.com',
+    subject: 'React JS Tutorial',
+    content: '<p>Hi there,</p><p>This is Vinod Kumar from Gollaprolu with not only the written word, but also an invitation to a live event full of tips on scheduling as a team!</p><p><h2>Tip of the Day… Live on January 1!</h2></p><p>We often share tips here for scheduling better as a team:</p><ul><li>Collective scheduling for bringing a coworker like a sales engineer or recruiter to a meeting (Professional plan and above)</li><li>Round Robin for sharing meeting opportunities with coworkers — like a pool of salespeople or customer-success pros (Teams plan and above)</li></ul><p>Thanks for reading,</p><p>Vinod Kumar</p><p>Head of Product Marketing @ Vinod</p>',
+    date: 'Dec 8',
+    isRead: true,
+    isArchived: false
+  },
+  {
+    id: 6,
+    name: 'Vinnu',
+    from: 'Vinnu@gmail.com',
+    content: '<p>Hi there,</p><p>This is Vinnu from USA with not only the written word, but also an invitation to a live event full of tips on scheduling as a team!</p><p><h2>Tip of the Day… Live on January 1!</h2></p><p>We often share tips here for scheduling better as a team:</p><ul><li>Collective scheduling for bringing a coworker like a sales engineer or recruiter to a meeting (Professional plan and above)</li><li>Round Robin for sharing meeting opportunities with coworkers — like a pool of salespeople or customer-success pros (Teams plan and above)</li></ul><p>Thanks for reading,</p><p>Vinod Kumar</p><p>Head of Product Marketing @ Vinod</p>',
+    subject: 'NodeJS Tutorial',
+    date: 'Dec 1',
+    isRead: true,
+    isArchived: false
+  },
+  {
+    id: 7,
+    name: 'Lisa',
+    from: 'Lisa@nyros.com',
+    subject: 'React JS Tutorial',
+    content: '<p>Hi there,</p><p>We’re excited to share that we will be participating in Collision 2025 in Toronto!</p><p>Collision brings together people and companies, redefining the global tech industry. Politico describes it as the ‘Olympics of Tech.’</p><p>Here’s what we have planned:</p><p>Stop by our booth #E222 (Hall B) to speak with our team, discuss your tech stack, and learn how you could receive up to $100K USD worth of Cloud credits on us (T&C’s apply).</p><p>Listen to our VP of Product Marketing, Raman Sharma, on ‘Building Great Products: Guided by Customer Feedback’ during the Masterclass on June 27th at 11:00 AM.</p><p>Our team will also be hosting office hours each day! Consider booking a 1:1 meeting with our team ahead of time to talk about how DigitalOcean can help your business succeed.</p><p>Not attending Collision 2023? Then sign up to join us at the DigitalOcean x CTO.ai Happy Hour on June 28th.</p><p>Register now</p><p>So mark your calendars. We look forward to seeing you there!</p><p>Swimmingly,</p><p>The DigitalOcean Team</p>',
+    date: 'Nov 28',
+    isRead: false,
+    isArchived: false
+  },
+  {
+    id: 8,
+    name: 'Google',
+    from: 'google.com',
+    subject: 'Privacy updates',
+    content: '<p>Hi there,</p><p>Hai Vinod Kumar we are implementing new features Explore it from  @Google</p>',
+    date: 'Nov 20',
+    isRead: false,
+    isArchived: false
+  }
+]
